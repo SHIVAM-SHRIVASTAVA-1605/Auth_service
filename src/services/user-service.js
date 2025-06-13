e3995d4a34db7c87dd50cc37a9b4e72e306c2efb
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2025-06-13 13:26:45 +0530

    Added function to compare the incoming password with execution password

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,5 @@
 const jwt  = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const UserRepository = require('../repository/user-repository');
 
@@ -38,6 +39,15 @@ class UserService {
             throw error;
         }
     }
+
+    checkPassword(userInputPlainPassword, encryptedPassword) {
+        try {
+            return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+        } catch (error) {
+            console.log("Something went wrong in password comparison");
+            throw error;
+        }
+    } 
 }
 
 module.exports = UserService;
