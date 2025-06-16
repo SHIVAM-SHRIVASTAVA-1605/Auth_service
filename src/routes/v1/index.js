54beeaf1af6b0a588a4252c98aa701058169661f
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2025-06-16 18:06:58 +0530

    Added isAdmin api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -22,4 +22,10 @@ router.get(
     UserController.isAuthenticated
 )
 
+router.get(
+    '/isAdmin',
+    AuthRequestValidator.validateIsAdminRequest,
+    UserController.isAdmin
+);
+
 module.exports = router;
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -87,7 +87,16 @@ class UserService {
             console.log("Something went wrong in password comparison");
             throw error;
         }
-    } 
+    }
+
+    isAdmin(userId) {
+        try {
+            return this.userRepository.isAdmin(userId);
+        } catch (error) {
+            console.log("something went wrong in service layer");
+            throw error;
+        }
+    }
 }
 
 module.exports = UserService;
